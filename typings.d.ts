f413394cf573e3fbafe2e23fe2571bd33ad388f2
Mwawaka <kedskeds90@gmail.com>
2022-07-20 17:10:03 +0300

    Update:Comment interface

--- typings.d.ts
+++ typings.d.ts
@@ -1,23 +1,39 @@
 // this is a definition typescript file that store type definitions
 
-export interface Post{
-    _id: string;
-    _createdAt: string;
-    title: string;
-    author: {
-        name: string;
-        image: image;
+export interface Post {
+  _id: string;
+  _createdAt: string;
+  title: string;
+  author: {
+    name: string;
+    image: image;
+  };
+    comments:[Comment]
+  description: string;
+  mainImage: {
+    asset: {
+      url: string;
+    };
+  };
+  slug: {
+    current: string;
+    body: [object];
+  };
+  body: [];
+}
 
-    };
-    description: string;
-    mainImage: {
-        asset: {
-            url: string;
-        };
-    };
-    slug: {
-        current: string;
-        body: [object];
-    }
-    body:[]
+export interface Comment {
+  _createdAt: string;
+  _id: string;
+  comment: string;
+  approved: boolean;
+  name: string;
+  email: string;
+  _rev: string;
+  _type: string;
+  _updatedAt: string;
+  post: {
+    _ref: string;
+    _type: string;
+  };
 }
